e973dfefea922944385ee00f2e47ee7c28f4de8a
Karan Patel <karanpatel4567@gmail.com>
2020-11-06 13:24:25 -0800

    Updating main_dynamo.py

--- aws/src/dynamo/main_dynamo.py
+++ aws/src/dynamo/main_dynamo.py
@@ -1,14 +1,18 @@
 from src.util import SessionUtil
 
+table_name = ''
 session = SessionUtil.get_session()
+
 dynamo_client = session.client('dynamodb')
+dynamodb_resource = session.resource('dynamodb')
+table = dynamodb_resource.Table(table_name)
 
 
-def describe_table(table_name):
+def describe_table(table_name: str) -> None:
     description = dynamo_client.describe_table(TableName=table_name)
 
     for key, value in description['Table'].items():
         print('\n{}:\n{}'.format(key, value))
 
 
-describe_table('table_name')
+describe_table(table_name)
